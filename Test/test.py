c4653aeaf305c0131a02519e8be34ae2949adb01
hcji <ji.hongchao@foxmail.com>
2019-05-19 17:15:40 +0800

    change test

---
+++ Test/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 19 17:06:01 2019
+
+@author: hcji
+"""
+
+from PyCFMID import fraggraph_gen, cfm_predict, cfm_id_database
+
+fragments = fraggraph_gen('CCCCN')
